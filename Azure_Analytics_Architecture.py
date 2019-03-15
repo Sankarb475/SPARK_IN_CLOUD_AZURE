a3c81bb22d655c151815be319b497b9087cc886d
Sankar Biswas <sankarb475@gmail.com>
2019-03-15 15:51:27 +0530

    Update and rename Azure_Analytics_Architecture.txt to Azure_Analytics_Architecture.py

--- Azure_Analytics_Architecture.py
+++ Azure_Analytics_Architecture.py
@@ -19,11 +19,9 @@ a pipeline architecture that aims to reduce the com‐ plexity seen in real-time
 incremental computation to only a small portion of this architecture.
 
 In lambda architecture, there are two paths for data to flow in the pipeline :
-• A “hot” path where latency-sensitive data (e.g., the results need to be ready in seconds or less) flows for rapid consumption
-by analytics clients. This data is mutable in nature.
+• A “hot” path where latency-sensitive data (e.g., the results need to be ready in seconds or less) flows for rapid consumption by analytics clients. This data is mutable in nature.
 
-• A “cold” path where all data goes and is processed in batches that can tolerate greater latencies (e.g., the results can take
-minutes or even hours) until results are ready. This is immutable in nature. Any changes in any already existing data would end
+• A “cold” path where all data goes and is processed in batches that can tolerate greater latencies (e.g., the results can take minutes or even hours) until results are ready. This is immutable in nature. Any changes in any already existing data would end
 up appending another row with a timestamp and changes accomodated
 
 
