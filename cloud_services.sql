76523d617ef25f9dcbb8f2cac6a0e7534c33a336
Sankar Biswas <sankarb475@gmail.com>
2021-09-03 13:42:29 +0530

    Create cloud_services.sql

---
+++ cloud_services.sql
@@ -0,0 +1,62 @@
+Azure
+==================
+AWS Glue - Data integration/ETL 
+--------------------------------------------
+-- AWS Glue is an event-driven, serverless computing platform provided by Amazon as a part of Amazon Web Services. It is a computing 
+service that runs code in response to events and automatically manages the computing resources required by that code.
+
+-- AWS Glue is a serverless data integration service that makes it easy to discover, prepare, and combine data for analytics, machine 
+learning, and application development.
+
+
+AWS Lambda - ETL tool
+---------------------------------------------
+AWS Lambda is an event-driven, serverless computing platform provided by Amazon as a part of Amazon Web Services. It is a computing 
+service that runs code in response to events and automatically manages the computing resources required by that code.
+
+
+Amazon Redshift - DWH 
+----------------------------------------------
+-- Based on MPP architecture
+
+
+Amazon RDS - Distributed relational database
+----------------------------------------------
+
+
+AWS Data Pipeline - webs service for processing and moving data between AWS services
+--------------------------------------------------------------------------------------------
+AWS Data Pipeline is a web service that helps you reliably process and move data between different AWS compute and storage services, 
+as well as on-premises data sources, at specified intervals. With AWS Data Pipeline, you can regularly access your data where it’s stored, 
+transform and process it at scale, and efficiently transfer the results to AWS services such as Amazon S3, Amazon RDS, Amazon DynamoDB, 
+and Amazon EMR.
+
+AWS Data Pipeline helps you easily create complex data processing workloads that are fault tolerant, repeatable, and highly available.
+ 
+
+Amazon Kinesis
+---------------------------------------------
+Amazon Kinesis makes it easy to collect, process, and analyze real-time, streaming data so you can get timely insights and react 
+quickly to new information.
+
+
+Amazon Glacier
+---------------------------------------------
+Amazon Glacier is an online file storage web service that provides storage for data archiving and backup.
+
+Glacier is part of the Amazon Web Services suite of cloud computing services, and is designed for long-term storage 
+of data that is infrequently accessed and for which retrieval latency times of 3 to 5 hours are acceptable
+
+
+
+Azure
+======================================================
+Azure eventHub - 
+---------------------------------------------
+Event Hubs is a fully managed, real-time data ingestion service that is simple, trusted and scalable. Stream millions of events per 
+second from any source to build dynamic data pipelines and immediately respond to business challenges.
+
+
+
+Difference between ETL and ELT
+======================================================
